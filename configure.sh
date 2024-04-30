60392e291d4f728e4ab5ebeb2817934b1445e391
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2024-04-30 15:28:55 +1200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,19 @@
+# Installed required tools from default repositories
+sudo apt-get install zenity nmap hashcat wfuzz hydra ffuf whatweb wafw00f cupp cewl crunch dirb gobuster htop lolcat wireshark sqlmap ruby-dev neofetch openvpn sublist3r
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+sudo gem install wpscan
+
+# Install Rust
+wget https://github.com/RustScan/RustScan/releases/download/2.2.2/rustscan_2.2.2_amd64.deb
+dpkg -i rustscan_2.2.2_amd64.deb
+
+# Install brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+sudo apt remove firefox-esr -y
+
 # Setting a new wallpaper
 wget -O ~/Pictures/background.jpg "https://pbs.twimg.com/media/EYUgEFkWsAE-Tkg?format=jpg&name=4096x4096"
 xfconf-query -c xfce4-desktop -l | grep last-image | xargs -I {} xfconf-query -c xfce4-desktop -p {} -s ~/Pictures/background.jpg && xfdesktop --reload
