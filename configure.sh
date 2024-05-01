891307d33078303ddb4882bb5e2fa538f046c016
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2024-05-01 22:33:48 +1200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -25,49 +25,6 @@ sudo apt remove firefox-esr -y
 
 # Setting a new wallpaper
 wget -O ~/Pictures/background.jpg "https://pbs.twimg.com/media/EYUgEFkWsAE-Tkg?format=jpg&name=4096x4096"
-xfconf-query -c xfce4-desktop -l | grep last-image | xargs -I {} xfconf-query -c xfce4-desktop -p {} -s ~/Pictures/background.jpg && xfdesktop --reload
-
-# Setting theme and icon theme
-xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Adwaita"
-
-xfconf-query -c xfce4-panel -p /panels -s 1 -t int -a
-xfce4-panel -r
-
-# Setup Terminal Visuals
-cat << EOF > ~/.config/xfce4/terminal/terminalrc
-[Configuration]
-BackgroundMode=TERMINAL_BACKGROUND_TRANSPARENT
-BackgroundDarkness=0.750000
-MiscAlwaysShowTabs=FALSE
-MiscBell=FALSE
-MiscBellUrgent=FALSE
-MiscBordersDefault=TRUE
-MiscCursorBlinks=FALSE
-MiscCursorShape=TERMINAL_CURSOR_SHAPE_BLOCK
-MiscDefaultGeometry=80x24
-MiscInheritGeometry=FALSE
-MiscMenubarDefault=TRUE
-MiscMouseAutohide=FALSE
-MiscMouseWheelZoom=TRUE
-MiscToolbarDefault=FALSE
-MiscConfirmClose=TRUE
-MiscCycleTabs=TRUE
-MiscTabCloseButtons=TRUE
-MiscTabCloseMiddleClick=TRUE
-MiscTabPosition=GTK_POS_TOP
-MiscHighlightUrls=TRUE
-MiscMiddleClickOpensUri=FALSE
-MiscCopyOnSelect=FALSE
-MiscShowRelaunchDialog=TRUE
-MiscRewrapOnResize=TRUE
-MiscUseShiftArrowsToScroll=FALSE
-MiscSlimTabs=FALSE
-MiscNewTabAdjacent=FALSE
-MiscSearchDialogOpacity=100
-MiscShowUnsafePasteDialog=TRUE
-MiscRightClickAction=TERMINAL_RIGHT_CLICK_ACTION_CONTEXT_MENU
-EOF
 
 # Setup John The Ripper
 sudo apt-get install libssl-dev -y
@@ -221,7 +178,3 @@ sudo rm msfinstall
 sudo wget https://raw.githubusercontent.com/pentestfunctions/escalation-folder/main/hosting_folder > /tmp/hosting_folder
 chmod +x /tmp/hosting_folder
 sudo mv /tmp/hosting_folder /bin/hostfolder
-
-cp ~/.bashrc ~/.bashrc.backup
-curl -o ~/.bashrc https://raw.githubusercontent.com/pentestfunctions/test-setup/main/.bashrc
-source ~/.bashrc
