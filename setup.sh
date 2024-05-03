c438390343476847f59032a94b6ea47664caa46a
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2024-05-03 16:29:17 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,52 @@
 #!/bin/bash
 
-# Removing specific packages
-sudo apt list --installed | grep -E 'libreoffice|xfburn|xsane|exfalso|parole|libetonyek|xarchiver|atril|thunderbird' | awk -F/ '{print $1}' | xargs sudo apt-get remove -y
+function setup_system() {
+    sudo apt update -y
+    sudo apt upgrade -y
 
-echo "$(ls /home)" | xargs -I {} sh -c 'echo "{} ALL=(ALL) ALL" >> /etc/sudoers'
+    grep -qxF 'deb http://http.kali.org/kali kali-rolling main non-free contrib' /etc/apt/sources.list || \
+    echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee -a /etc/apt/sources.list > /dev/null
+    
+    wget -qO- https://archive.kali.org/archive-key.asc | sudo tee /etc/apt/trusted.gpg.d/archive-key.asc
+    sudo apt update
 
-# Run a script from the internet
-wget https://raw.githubusercontent.com/wonghoi/enhanced_session_linux/main/setup_enhanced_session.sh -O - | sh
+    local content=$(cat <<-END
+Package: *
+Pin: release a=kali-rolling
+Pin-Priority: 100
+END
+    )
+    
+    local file="/etc/apt/preferences.d/kali.pref"
+
+    if ! grep -qxF "$content" "$file"; then
+        echo "$content" | sudo tee -a "$file" > /dev/null
+        echo "Content added to $file"
+    else
+        echo "Content already exists in $file"
+    fi
+}
+
+function configure_services() {
+    sudo apt install xrdp kali-tweaks -y
+    apt-get install --yes pipewire-module-xrdp
+    /usr/lib/kali_tweaks/helpers/hyperv-enhanced-mode enable
+    systemctl start xrdp
+    systemctl enable xrdp
+
+    local sudoers_file="/etc/sudoers"
+
+    for user in /home/*; do
+        local username=$(basename "$user")
+
+        if ! grep -q "^$username " "$sudoers_file"; then
+            echo "$username ALL=(ALL) NOPASSWD: ALL" >> "$sudoers_file"
+        fi
+    done
+
+    sudo poweroff
+}
+
+# Call the functions
+setup_system
+configure_services
